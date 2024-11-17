440d2b0557fe8cf6230261d5125d9f96cb6d1d80
unknown <monjan87@gmail.com>
2024-11-17 15:00:18 +0100

    2

--- Pandas_csv.py
+++ Pandas_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('weight-height.csv', delimiter=";")
 
@@ -17,9 +17,38 @@ print('\nWykres')
 plt.hist(df.Weight, 50)
 plt.show()
 
+plt.hist(df.query('Gender=="Male"').Weight, 50)
+#plt.show()
+
+plt.hist(df.query('Gender=="Female"').Weight, 50)
+plt.show()
+
+#biblioteka seaborn
+sns.histplot(df.query('Gender=="Male"').Weight)
+sns.histplot(df.query('Gender=="Female"').Weight)
+plt.show()
 
 #print(df.describe())
 
+#zamiana gender na dane numeryczne
 
+df = pd.get_dummies(df)
+print(df.head(3))
+#del df.Gender_Male   #jednne pythonowe
+#słowko to nie pozwoli
+del df['Gender_Male']
+print(df.head(3))
 
+df = df.rename(columns={'Gender_Female': 'Gender'})
+print(df.head(3))
+#false - mężczyzna, true - kobieta
+
+#algorytm maszynlerningowa czesc
+
+model = LinearRegression()
+model.fit(df[['Height', 'Gender']] , df['Weight']   )
+print(model.coef_)   #współczynnik kierunkowy -
+print(model.intercept_) #wyraz wolny
+
+print(f'Weight = Height * {model.coef_[0]} + Gender * {model.coef_[1]} + {model.intercept_}')
 
